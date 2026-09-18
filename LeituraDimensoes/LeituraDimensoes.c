5b6d806c8ac1be736163a5ad2389ae14e5197bce
DinisAzevedo <pedrodinisazevedo08@gmail.com>
2026-09-18 15:20:14 +0100

    Changes

---
+++ LeituraDimensoes/LeituraDimensoes.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    float l1;
+    float l2;
+
+    printf("Altura? ");
+    scanf("%f", &l1);
+
+    printf("Largura? ");
+    scanf("%f", &l2);
+
+    return 0;
+}
+
+// gcc LeituraDimensoes.c - o LeituraDimensoes.exe >>.\LeituraDimensoes.exe
