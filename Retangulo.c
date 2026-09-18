8046824a10d057a5c33dd74e08cc12ca62e9d50e
DinisAzevedo <pedrodinisazevedo08@gmail.com>
2026-09-18 14:46:54 +0100

    First Commit

--- Retangulo.c
+++ Retangulo.c
@@ -1,7 +1,13 @@
 // calculo do perimetro e area de um retangulo
 #include <stdio.h>
-int main(){
-    l1 = 3.7;
-    l2
+void main(){
+    float l1 = 3.7;
+    float l2 = 7.2;
+
+    float a = l1 * l2;
+    float p = 2*(l1+l2);
+
+    printf ("area = %.2f\n", a);
+    printf ("perimetro = %.2f\n", p);
     
 }
